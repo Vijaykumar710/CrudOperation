a8d51f2d3bc427abda582376b9cbcd90973bdc3e
Vijaykumar710 <106771225+Vijaykumar710@users.noreply.github.com>
2023-03-12 15:06:21 +0530

    Update StudentController.java

--- src/main/java/com/curdoperation/controller/StudentController.java
+++ src/main/java/com/curdoperation/controller/StudentController.java
@@ -20,8 +20,6 @@ public class StudentController {
     @Autowired
     StudentService studentService;
 
-/////=====================================NEW Repository========================================================/////////////
-
 
     @PostMapping("/students")
     public ResponseEntity<Student> createStudent(@RequestBody Student student) {
@@ -64,63 +62,8 @@ public class StudentController {
             return new ResponseEntity<>((HttpStatus.NOT_FOUND));
         }
     }
-    //=========================================Get,post by Service class==============================================================//
+  
 
-    @PostMapping("/student")
-    public void addStudent(@RequestBody Student student) {
-        studentService.add(student);
-    }
 
-    @GetMapping("/getall")
-    public List<Student> getAllStudent() {
-        return studentService.getAllStudent();
-    }
-
-    @GetMapping("/{id}")
-    public Student getByID(@PathVariable long id) {
-        return studentService.getById(id);
-    }
-
-    @PutMapping("/student/{id}")
-    public Student updateStudent(@RequestBody Student student, @PathVariable Long id) {
-        return studentService.updateStudent(student, id);
-    }
-
-    @DeleteMapping("/delete/{id}")
-    public void deleteById(@PathVariable Long id) {
-        studentService.deleteById(id);
-    }
-
-  /*  @DeleteMapping("/delrepo/{id}")
-    public void deleteByIDRepo(@PathVariable Long id){
-        studentRepository.deleteById(id);
-    }*/
-
-    /* @GetMapping("/stud/{id}")
-      public Student getByIDStudent(@PathVariable Long id) {
-          return studentService.getStudentById(id);
-      }*/
-    /////===============================Message=============================================================================////////
-    @PostMapping("/studentmessage")
-    public ResponseEntity<ResponseMessage> createStudentMessage(@RequestBody Student student) {
-        String message = "";
-        Student saveStudent = studentRepository.save(student);
-        message = "created the Id: " + saveStudent.getId();
-        return ResponseEntity.status(HttpStatus.OK).body(new ResponseMessage(message));
-    }
 }
-/*Total update without writing in service class
-    @PutMapping("/update/{id}")
-    public ResponseEntity<Student> updateByID(@RequestBody Student student,@PathVariable long id) {
-        Optional<Student> studentID = studentRepository.findById(id);
-        if(studentID.isPresent()){
-            Student _student = studentID.get();
-            _student.setName(student.getName());
-            _student.setId(student.getId());
-            _student.setRole(student.getRole());
-            _student.setNumber(student.getNumber());
-            return new ResponseEntity<>(studentRepository.save(_student), HttpStatus.OK);
-        }else{
-            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }*/
+
